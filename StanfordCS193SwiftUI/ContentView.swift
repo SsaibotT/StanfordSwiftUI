3e3ab613c335217bf9d3665df4480bad0cb43e46
Serhii <1SsaibotT1@gmail.com>
2020-07-19 01:04:40 +0300

    Initial Commit

---
+++ StanfordCS193SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StanfordCS193SwiftUI
+//
+//  Created by Serhii Semenov on 19.07.2020.
+//  Copyright © 2020 Serhii Semenov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
